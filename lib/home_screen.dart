d2421ec50804b759ff59f7114a527958e9e80a8e
Lee Joo Won <first0627@hanmail.net>
2023-04-24 00:49:54 +0900

    timer

---
+++ lib/home_screen.dart
@@ -0,0 +1,135 @@
+import 'dart:async';
+
+import 'package:flutter/material.dart';
+
+class HomeScreen extends StatefulWidget {
+  const HomeScreen({super.key});
+
+  @override
+  State<HomeScreen> createState() => _HomeScreenState();
+}
+
+class _HomeScreenState extends State<HomeScreen> {
+  static const twentyFiveMinutes = 1500;
+  bool isRunning = false;
+  int totalSeconds = twentyFiveMinutes;
+  late Timer timer;
+  int totalPomodoros = 0;
+
+  void onTick(Timer timer) {
+    if (totalSeconds == 0) {
+      setState(() {
+        totalPomodoros = totalSeconds + 1;
+        isRunning = false;
+        totalSeconds = twentyFiveMinutes;
+      });
+      timer.cancel();
+    } else {
+      setState(() {
+        totalSeconds = totalSeconds - 1;
+      });
+    }
+  }
+
+  void onStartPressed() {
+    timer = Timer.periodic(
+      const Duration(seconds: 1),
+      onTick,
+    ); //Timer가 실행 해줄거임 뒤에 onTick() 하면 지금 당장 실행하라는거니까 그러면 안댐
+    setState(() {
+      isRunning = true;
+    });
+  }
+
+  void onPausePressed() {
+    timer.cancel();
+    setState(() {
+      isRunning = false;
+    });
+  }
+
+  String format(int seconds) {
+    var duration = Duration(seconds: seconds);
+
+    return duration.toString().split(".").first.substring(2, 7);
+    //split하면 해당 구분자 . 을 기준으로 나눠주고 first는 그 중 앞엣것을 의미한다.
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: Theme.of(context).colorScheme.background,
+      body: Column(
+        children: [
+          Flexible(
+            flex: 1, //하나의 박스가 얼만큼의 크기를 차지 하는지 비율
+            child: Container(
+              alignment: Alignment.bottomCenter,
+              child: Text(
+                format(totalSeconds),
+                style: TextStyle(
+                  color: Theme.of(context).cardColor,
+                  fontSize: 89,
+                  fontWeight: FontWeight.w600,
+                ),
+              ),
+            ),
+          ),
+          Flexible(
+            flex: 3,
+            child: Center(
+              child: IconButton(
+                color: Theme.of(context).cardColor,
+                iconSize: 120,
+                onPressed: isRunning ? onPausePressed : onStartPressed,
+                icon: Icon(isRunning
+                    ? Icons.pause_circle_outlined
+                    : Icons.play_circle_outlined),
+              ),
+            ),
+          ),
+          Flexible(
+            flex: 1,
+            child: Row(
+              children: [
+                Expanded(
+                  child: Container(
+                    decoration: BoxDecoration(
+                      color: Theme.of(context).cardColor,
+                      borderRadius: BorderRadius.circular(50),
+                    ),
+                    child: Column(
+                      mainAxisAlignment: MainAxisAlignment.center,
+                      children: [
+                        Text(
+                          'Pomodors',
+                          style: TextStyle(
+                              fontSize: 20,
+                              fontWeight: FontWeight.w600,
+                              color: Theme.of(context)
+                                  .textTheme
+                                  .displayLarge!
+                                  .color),
+                        ),
+                        Text(
+                          '$totalPomodoros',
+                          style: TextStyle(
+                              fontSize: 85,
+                              fontWeight: FontWeight.w600,
+                              color: Theme.of(context)
+                                  .textTheme
+                                  .displayLarge!
+                                  .color),
+                        ),
+                      ],
+                    ),
+                  ),
+                ),
+              ],
+            ),
+          ),
+        ],
+      ),
+    );
+  }
+}
--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:toonflix/home_screen.dart';
 
 const taxAmount = 15;
 const priceAmount = 30;
@@ -8,94 +9,27 @@ void main() {
 
 class App extends StatefulWidget {
   const App({super.key});
-  //여기의 데이터는 클래스 프로퍼티이다.\
+
   @override
   State<App> createState() => _AppState();
 }
 
 class _AppState extends State<App> {
-  //BuildContext는 모든 상위 요소들에 대한 정보야
-  //즉 위젯 트리에 대한 정보가 담겨있지
-  //매우 먼 요소의 데이터를 가져 올 수 있기 때문에 아주 유용하다.
-  //이게 context가 하는것이다.
-
-  bool showTitle = true;
-  void toggleTitle() {
-    setState(() {
-      showTitle = !showTitle;
-    });
-  }
-
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
       theme: ThemeData(
+        colorScheme: ColorScheme.fromSwatch(
+          backgroundColor: const Color(0xFFE7626C),
+        ),
         textTheme: const TextTheme(
-          titleLarge: TextStyle(
-            color: Colors.red,
+          displayLarge: TextStyle(
+            color: Color(0xFF232B55),
           ),
         ),
+        cardColor: const Color(0xFFF4EDDB),
       ),
-      home: Scaffold(
-        backgroundColor: const Color(0xFFF4EDDB),
-        body: Center(
-          child: Column(
-            mainAxisAlignment: MainAxisAlignment.center,
-            children: [
-              showTitle ? const MyLargeTitle() : const Text('nothing'),
-              IconButton(
-                onPressed: toggleTitle,
-                icon: const Icon(Icons.remove_red_eye),
-              )
-            ],
-          ),
-        ),
-      ),
-    );
-  }
-}
-
-class MyLargeTitle extends StatefulWidget {
-  const MyLargeTitle({
-    super.key,
-  });
-
-  @override
-  State<MyLargeTitle> createState() => _MyLargeTitleState();
-}
-
-class _MyLargeTitleState extends State<MyLargeTitle> {
-  int count = 0; //대부분 상태변수를 여기에 선언한다.
-
-  @override
-  void initState() {
-    super.initState();
-    print('initState!');
-
-    //가끔 부모 요소에 의존하는 데이터를 초기화해야하는 경우가 있다.
-    //가끔 데이터를 초기화하기 위 context를 사용함
-    //종종 API에서 업데이트를 구독하고 싶음
-    //항상 build 메서드 전에 실행된다.
-    //api어데이트나 이벤트 리스너로 부터 구독 취소하거나 form의 리스너로 부터 벗어나고 싶을때
-    //우리의 위젯이 위젯 트리에서 제거되기 전에 뭔가 하고 싶을때
-  }
-
-  @override
-  void dispose() {
-    //위젯이 스크린에서 제거될 때 호출되는 메소드
-    super.dispose();
-    print("dispose!");
-  }
-
-  @override
-  Widget build(BuildContext context) {
-    print('build!');
-    return Text(
-      'My Large Title',
-      style: TextStyle(
-        fontSize: 30,
-        color: Theme.of(context).textTheme.titleLarge?.color,
-      ),
+      home: const HomeScreen(),
     );
   }
 }
