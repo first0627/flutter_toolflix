bb70078aaa4984e985590e2adea1928c0deeee9a
Lee Joo Won <first0627@hanmail.net>
2023-04-25 21:25:19 +0900

    OnPause

--- lib/home_screen.dart
+++ lib/home_screen.dart
@@ -48,6 +48,15 @@ class _HomeScreenState extends State<HomeScreen> {
     });
   }
 
+  void onStopPressed() {
+    timer.cancel();
+    setState(() {
+      totalSeconds = twentyFiveMinutes;
+      totalPomodoros = 0;
+      isRunning = false;
+    });
+  }
+
   String format(int seconds) {
     var duration = Duration(seconds: seconds);
 
@@ -62,30 +71,40 @@ class _HomeScreenState extends State<HomeScreen> {
       body: Column(
         children: [
           Flexible(
-            flex: 1, //하나의 박스가 얼만큼의 크기를 차지 하는지 비율
-            child: Container(
-              alignment: Alignment.bottomCenter,
-              child: Text(
-                format(totalSeconds),
-                style: TextStyle(
-                  color: Theme.of(context).cardColor,
-                  fontSize: 89,
-                  fontWeight: FontWeight.w600,
+            flex: 2, //하나의 박스가 얼만큼의 크기를 차지 하는지 비율
+            child: Column(
+              mainAxisAlignment: MainAxisAlignment.end,
+              children: [
+                Text(
+                  format(totalSeconds),
+                  style: TextStyle(
+                      color: Theme.of(context).cardColor,
+                      fontSize: 89,
+                      fontWeight: FontWeight.w600),
                 ),
-              ),
+                IconButton(
+                  onPressed: onStopPressed,
+                  icon: const Icon(Icons.restart_alt),
+                  iconSize: 50,
+                  color: Theme.of(context).cardColor,
+                ),
+              ],
             ),
           ),
           Flexible(
             flex: 3,
-            child: Center(
-              child: IconButton(
-                color: Theme.of(context).cardColor,
-                iconSize: 120,
-                onPressed: isRunning ? onPausePressed : onStartPressed,
-                icon: Icon(isRunning
-                    ? Icons.pause_circle_outlined
-                    : Icons.play_circle_outlined),
-              ),
+            child: Column(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: [
+                IconButton(
+                  iconSize: 120,
+                  color: Theme.of(context).cardColor,
+                  onPressed: isRunning ? onPausePressed : onStartPressed,
+                  icon: Icon(isRunning
+                      ? Icons.pause_circle_outline
+                      : Icons.play_circle_outline),
+                ),
+              ],
             ),
           ),
           Flexible(
