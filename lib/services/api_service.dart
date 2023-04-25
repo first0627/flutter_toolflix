6f2c73679e0354585a3a5e6313d7b2c8a9b8a1a8
Lee Joo Won <first0627@hanmail.net>
2023-04-25 22:31:46 +0900

    04251031webtoon

---
+++ lib/services/api_service.dart
@@ -0,0 +1,20 @@
+import 'package:http/http.dart' as http;
+
+class ApiService {
+  final String baseUrl = "https://webtoon-crawler.nomadcoders.workers.dev";
+  final String today = "today";
+
+  //여기서  await한 다음에 응답이 와야지 다음 코드가 실행된다.
+  //await은 비동기함수이고 이 비동기함수내에서는 코드가 동기처럼 await이 끝나야 다음코드가 실행되지만
+  //이 함수 뿐 아니라 프로그램 전체적인 실행의 방면에서는 비동기다. 즉 해당함수 말고 다른 코드의 흐름은 먼저 작동한다는 뜻
+  void getTodaysToons() async {
+    final url = Uri.parse('$baseUrl/$today');
+    final reponse =
+        await http.get(url); //return 타입은 Future이다. 미래에 받을 값의 타입을 알려준다.
+    if (reponse.statusCode == 200) {
+      print(reponse.body); //여기에 서버에서 보낸 데이터가 들어있어.
+      return;
+    }
+    throw Error();
+  }
+}
