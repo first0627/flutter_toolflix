d7823732d2c8c109e3e76021bea4b0ea91672f0b
Lee Joo Won <first0627@hanmail.net>
2023-05-01 23:39:43 +0900

    homescreen_Finish

--- lib/screens/home_screen.dart
+++ lib/screens/home_screen.dart
@@ -27,16 +27,13 @@ class HomeScreen extends StatelessWidget {
         future: webtoons,
         builder: (context, snapshot) {
           if (snapshot.hasData) {
-            return ListView.separated(
-              scrollDirection: Axis.horizontal,
-              itemCount: snapshot.data!.length,
-              itemBuilder: (context, index) {
-                var webtoon = snapshot.data![index];
-                return Text(webtoon.title);
-                //listview builder는 사용자가 보고 있는 섹션만 보여줌
-              },
-              separatorBuilder: (context, index) => const SizedBox(width: 20),
-              //data뒤에 !는 null safetyㅎㅎ
+            return Column(
+              children: [
+                const SizedBox(
+                  height: 50,
+                ),
+                Expanded(child: makList(snapshot))
+              ],
             );
           }
           return const Center(
@@ -44,6 +41,56 @@ class HomeScreen extends StatelessWidget {
           );
         },
       ),
+    );
+  }
+
+  ListView makList(AsyncSnapshot<List<WebtoonModel>> snapshot) {
+    return ListView.separated(
+      scrollDirection: Axis.horizontal,
+      itemCount: snapshot.data!.length,
+      padding: const EdgeInsets.symmetric(
+          vertical: 10, horizontal: 20), //data뒤에 !는 null safetyㅎㅎ
+      itemBuilder: (context, index) {
+        var webtoon = snapshot.data![index];
+        return Column(
+          children: [
+            Container(
+              width: 250,
+              clipBehavior: Clip.hardEdge, //자식의 부모 영역 침범을 제어하는 방법
+              decoration: BoxDecoration(
+                borderRadius: BorderRadius.circular(15),
+                boxShadow: [
+                  BoxShadow(
+                    blurRadius: 15,
+                    offset: const Offset(10, 10), //태양의 위치를 바꾼다고 생각하면된다.
+                    color: Colors.black.withOpacity(0.5),
+                  ),
+                ],
+              ),
+              child: Image.network(
+                webtoon.thumb,
+                headers: const {
+                  "User-Agent":
+                      "Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/110.0.0.0 Safari/537.36", //강의에서 오류떠서 아래 해결책 참고해서 넣은 코드
+                },
+              ),
+            ),
+            const SizedBox(
+              height: 10,
+            ),
+            Text(
+              webtoon.title,
+              style: const TextStyle(
+                fontSize: 22,
+                fontWeight: FontWeight.w600,
+              ),
+            )
+          ],
+        );
+        //listview builder는 사용자가 보고 있는 섹션만보여줌
+      },
+      separatorBuilder: (context, index) => const SizedBox(width: 40),
+      //구분자 추가
     );
   }
 }
