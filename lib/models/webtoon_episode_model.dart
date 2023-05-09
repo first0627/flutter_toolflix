ffde3256646d7e77df09a9b967538cc51962e393
Lee Joo Won <first0627@hanmail.net>
2023-05-09 22:42:23 +0900

    05092242

---
+++ lib/models/webtoon_episode_model.dart
@@ -0,0 +1,9 @@
+class WebtoonEpisodeModel {
+  final String id, title, raitng, date;
+
+  WebtoonEpisodeModel.fromJson(Map<String, dynamic> json)
+      : id = json["id"],
+        title = json["title"],
+        raitng = json["raitng"],
+        date = json["date"];
+}
